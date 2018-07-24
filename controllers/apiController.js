d20591a980de49e9c2bdeed0e19fc3291ec2fb32
kmocorro <kevinmocorro.github@gmail.com>
2018-07-24 13:26:07 +0800

    add new ft index

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -109,7 +109,33 @@ module.exports = function(app){
 
                 feed().then(function(data){
 
-                    res.render('index', {username: req.claim.username, department: req.claim.department, data});
+                    function getGreetingTime (m) {
+                        var g = null; //return g
+                        
+                        if(!m || !m.isValid()) { return; } //if we can't find a valid or filled moment, we return.
+                        
+                        var split_afternoon = 12 //24hr time to split the afternoon
+                        var split_evening = 17 //24hr time to split the evening
+                        var currentHour = parseFloat(m.format("HH"));
+                        
+                        if(currentHour >= split_afternoon && currentHour <= split_evening) {
+                            g = "afternoon";
+                        } else if(currentHour >= split_evening) {
+                            g = "evening";
+                        } else {
+                            g = "morning";
+                        }
+                        
+                        return g;
+                    }
+                    
+                    
+                    //The var "humanizedGreeting" below will equal (assuming the time is 8pm) "Good evening, James."
+                        
+                    var humanizedGreeting = "Good " + getGreetingTime(moment()) + ", " +  req.claim.username + ".";
+                    
+
+                    res.render('index', {username: req.claim.username, greet: humanizedGreeting, department: req.claim.department, data});
                     connection.release();
 
                 },  function(err){
