c66a6de431742b8f59ebe1013276de4f89a65b92
kmocorro <kevinmocorro.github@gmail.com>
2018-08-28 16:55:20 +0800

    bug fixed verify

--- auth/verifyToken.js
+++ auth/verifyToken.js
@@ -18,26 +18,22 @@ function verifyToken(req, res, next){
             }
         }, config.secret, { expiresIn: 300 });
 
-        return res.status(200).render('login', {authenticity_token});
+        return res.render('login', {authenticity_token});
     } else {
         // cookies.auth is available, verify.
         jwt.verify(token, config.secret, function(err, decoded){
 
             if(err){
-                    
-                let authenticity_token = jwt.sign({
-                id: uuidv4(),
-                claim: {
-                    signup: 'valid'
-                }
-            }, config.secret, { expiresIn: 300 });
-
-            return res.redirect('login', {authenticity_token});
+                
+                let authenticity_token = jwt.sign({id: uuidv4(), claim:{ signup: 'valid'}}, config.secret, {expiresIn:300});
+                return res.status(200).render('login', {authenticity_token});
 
             } else {
+
                 req.userID = decoded.id;
                 req.claim = decoded.claim;
                 next();
+
             }
 
         });
